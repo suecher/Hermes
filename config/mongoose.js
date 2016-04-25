1f31a8b816ba704d1b29295be7eff1a68c020f1a
suecher <suecher@qq.com>
2016-04-25 20:42:32 +0800

    修改文件名大小写

--- config/mongoose.js
+++ config/mongoose.js
@@ -7,13 +7,12 @@ var config = require('./config');
 module.exports = function(){
     var db = mongoose.connect(config.mongodb);
     require('../app/models/user.server.model');
-    require('../app/models/archeriesscore.server.model.js');
+    require('../app/models/archeriesscore.server.model');
     require('../app/models/club.server.model');
     require('../app/models/friend.server.model');
-    require('../app/models/honorbyuser.server.model.js');
     require('../app/models/message.server.model.js');
-    require('../app/models/clubfollow.server.model.js');
-    require('../app/models/advert.server.model.js');
-    require('../app/models/challenge.server.model.js');
+    require('../app/models/clubfollow.server.model');
+    require('../app/models/advert.server.model');
+    require('../app/models/challenge.server.model');
     return db;
 };

fa44aa75a0acd4a160b958be7173fa59a670975c
scan.home.pc <suecher@live.com>
2016-07-07 02:47:44 +0800

    城市活动获取接口中 加载平台活动
    城市活动获取接口中 加载平台活动

--- app/controllers/advert.server.controller.js
+++ app/controllers/advert.server.controller.js
@@ -32,14 +32,27 @@ module.exports = {
     },
     advertQuery:function(obj,callback){
         if(obj.clubId || obj.city){
-            Advert.find(obj,function(err,docs){
-                if(err){
-                    callback(resultobj.createResult(false,'SelectAdvertError','查询广告信息出错,'));
-                    return;
-                }
 
-                callback(resultobj.createResult(true,null,null,docs));
-            });
+            //city true 证明活动界面获取数据。这个时候需要获取俱乐部置顶的消息
+            if(obj.city){
+                Advert.find({ "$or":[{"city":obj.city}, {"clubId":"000000000000000000000001"}]},function(err,docs){
+                    if(err){
+                        callback(resultobj.createResult(false,'SelectAdvertError','查询广告信息出错,'));
+                        return;
+                    };
+
+                    callback(resultobj.createResult(true,null,null,docs));
+                });
+            } else {
+                Advert.find(obj,function(err,docs){
+                    if(err){
+                        callback(resultobj.createResult(false,'SelectAdvertError','查询广告信息出错,'));
+                        return;
+                    };
+
+                    callback(resultobj.createResult(true,null,null,docs));
+                });
+            }
         } else {
             callback(resultobj.createResult(false,'Required parameter missing','缺少必要信息,'));
         }
